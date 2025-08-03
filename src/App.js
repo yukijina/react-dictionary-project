dedd32ce033ce3bea29b053eed5441b29a766f6c
yuki <38411009+yukijina@users.noreply.github.com>
2025-08-03 14:22:04 -0700

    imported font family, updated css, added footer

--- src/App.js
+++ src/App.js
@@ -7,11 +7,35 @@ function App() {
     <div className='App'>
       <header className='App-header'>
         <img src={logo} alt='dictionary logo' className='App-logo' />
-        <h1>My Dictionary</h1>
+        <h1>Dictionary</h1>
       </header>
-      <main>
+      <main className='App-main-container'>
+        <p className='App-title'>What word do you want to look up?</p>
         <Dictionary />
       </main>
+      <hr />
+      <footer className='App-footer'>
+        <p className='App-footer-text'>
+          This project was coded by Yukiji and is open-sourced on{' '}
+          <a
+            href='https://github.com/yukijina/react-dictionary-project'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='App-footer-link'
+          >
+            GitHub
+          </a>{' '}
+          and hosted on{' '}
+          <a
+            href='https://tourmaline-bunny-react-dictionary.netlify.app/'
+            target='_blank'
+            rel='noopener noreferrer'
+            className='App-footer-link'
+          >
+            Netlify
+          </a>{' '}
+        </p>
+      </footer>
     </div>
   );
 }
